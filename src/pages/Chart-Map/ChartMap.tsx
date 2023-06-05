b006a1bcbafc8fedb8cd28d5a2d1e60dce8a4ce0
Jose Silva <ernerdo@gmail.com>
2023-06-04 18:05:18 -0600

    config react query

--- src/pages/Chart-Map/ChartMap.tsx
+++ src/pages/Chart-Map/ChartMap.tsx
@@ -1,6 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
 import { DefaultLayout } from '../../layout'
 
 export const ChartMap = () => {
+  const { status, data, error, isFetching } = useQuery(['chartMap'], () =>
+    fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=all').then(
+      (res) => res.json()
+    )
+  )
+  console.log(status, data, error, isFetching)
+
   return (
     <DefaultLayout>
       <div>
